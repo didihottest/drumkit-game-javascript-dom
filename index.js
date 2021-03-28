e06115161a220bcdc91df28f3a69f63246a28905
didihottest <didihottest@gmail.com>
2021-03-28 15:27:40 +0700

    drum-kit game

---
+++ index.js
@@ -0,0 +1,74 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+function playSound(buttonPressed) {
+    
+    switch (buttonPressed) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+for (var i=0; i<numberOfButtons; i++){document.querySelectorAll("button")[i].addEventListener("click", function(){
+        
+        var buttonPressed = this.innerHTML;           
+        playSound(buttonPressed);
+        
+        animationPress(buttonPressed);
+
+    })
+
+   
+}
+
+document.addEventListener("keypress", function(event){
+                
+    playSound(event.key);
+    animationPress(event.key);
+
+})
+
+function animationPress (buttonPressed) {
+    var activeButton = document.querySelector("." + buttonPressed);
+    
+    activeButton.classList.add("pressed");
+   
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, 100);
+    
+}
